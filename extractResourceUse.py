4f339cae0d2e5397e016e0e9305e5c25b0684a9a
anniewang-27 <106289217+anniewang-27@users.noreply.github.com>
2024-10-24 14:57:56 -0400

    added code from 1b, removed the progress bar

--- extractResourceUse.py
+++ extractResourceUse.py
@@ -1,8 +1,11 @@
-from utils.py import Counter
+from utils import Counter
 
-import rpy2.robjects as robjects
+import rpy2.robjects as robjects, pandas2ri
 from rpy2.robjects.packages import importr, data
 from rpy2.robjects.vectors import StrVector
+
+import pandas as pd
+import numpy as np
 
 # from https://rviews.rstudio.com/2022/05/25/calling-r-from-python-with-rpy2/
 utils = importr('utils')
@@ -24,11 +27,12 @@ library("finalfit")
 robjects.r('gc()')
 robjects.r('PREFIX_PATH = "~/data"')
 
+# puts csawesome_datashop.txt file into R data table named csawesome
 robjects.r('''
 csawesome <- fread(paste0(PREFIX_PATH, '/csawesome_datashop.txt'), verbose=TRUE)
-runestone_summary <- read.csv(paste0(PREFIX_PATH, '/complete_course_summary_all_students_12_18.csv'))
 ''')
 
+# rearranges the csawesome data table, adds more columns
 robjects.r('''
 csawesome <- csawesome %>%
   dplyr::arrange(`Anon Student Id`, Time, .by_group=TRUE) %>%
@@ -40,8 +44,21 @@ csawesome <- csawesome %>%
 
 csawesome <- csawesome[, c('Anon Student Id', 'Problem Name', 'Level (Chapter)', 'Level (SubChapter)', 'time_diff_s', 'Selection', 'Action', 'Input', 'Feedback Classification')]
 ''')
-
 csawesome = robjects.r('csawesome')
-print(csawesome)
 
+# --------------------------- 1b. extract resource use
+df = pandas2ri.ri2py(csawesome)
 
+counter = Counter(0)
+
+# initialize new columns and iterate over the student_list to increment the counters
+df.loc[:,'page_counter'] = pd.NA
+df.loc[:,'video_counter'] = pd.NA
+df.loc[:,'act_counter'] = pd.NA
+
+student_list = list(df['Anon Student Id'].unique())
+
+for student in student_list:
+  df[df['Anon Student Id'] == student] = df[df['Anon Student Id'] == student].apply(counter.increment_counter,axis=1)
+
+df.to_csv('~/data/all_students_wcounters.csv',index=False)
